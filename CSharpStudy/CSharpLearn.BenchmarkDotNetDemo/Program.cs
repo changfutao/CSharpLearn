fa67e4c53f3d47e51b456c2e2ebfe67db8e82af6
changfutao <changfutao@126.com>
2024-12-04 15:05:47 +0800

    benchmark demo

---
+++ CSharpStudy/CSharpLearn.BenchmarkDotNetDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace CSharpLearn.BenchmarkDotNetDemo
+{
+    /// <summary>
+    /// Benchmark注意事项:
+    /// 1. 要使用有编译器优化的Release模式
+    /// 2. 被测试的类、使用了特性的方法与字段均需要为public
+    /// 3. 在要测试的方法中尽量避免会被JIT优化点的情况,比如有一个不会被使用的变量等
+    /// 4. 除非还想要测试内存读取的速度等,否则一般没有必要创建过大的数组
+    /// 
+    /// > Class
+    ///     MemoryDiagnoser: 查看内存分配情况(有一个bool参数,表示是否显示GC的情况)
+    ///     SimpleJob: 可以设置RuntimeMoniker.Net60
+    ///     Orderer(SummaryOrderPolicy.SlowestToFastest): 输出结果排序
+    ///     RankColumn: 为结果表格添加一列Rank, 表示当前行的方法的排名
+    /// > Method
+    ///     Benchmark: 表示这个方法需要被测试 (另有一个Baseline参数,同时会给结果添加一列Ratio, 表示和Baseline的比率)
+    ///     Arguments: 类似于Params, 表示该方法的传参,可以有多个,并且会和Params联动,充分考虑各种组合
+    ///     GlobalSetup: 全局初始化, 常用于初始化一个要用来测试的变量、集合等。可以和Params联动,比如数组的容量由某个字段决定
+    ///     IterationSetup: 用于在每次迭代前的初始化,每次迭代都会调用一次
+    /// > Field
+    ///     Params: 某个字段可能有不同的值 (如果多个字段被标记该特性,则会充分考虑所有参数的组合)
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<SimpleDemo>();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpStudy/CSharpLearn.BenchmarkDotNetDemo/SimpleDemo.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace CSharpLearn.BenchmarkDotNetDemo
+{
+    [SimpleJob(RuntimeMoniker.Net80)] // 使用.net8
+    [MemoryDiagnoser(false)]
+    [RankColumn]
+    [Orderer]
+    
+    public class SimpleDemo
+    {
+        [Params(4, 16, 130)] // 三种参数情况
+        public int count; // 这里既支持字段也支持属性
+        public List<int> list;
+
+        [GlobalSetup]
+        public void Init()
+        {
+            
+        }
+
+        [Benchmark(Baseline = true)] // 测试
+        public List<int> ListInit()
+        {
+            list = new List<int>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(i);
+            }
+            return list;
+        }
+
+        [Benchmark] // 测试
+        public List<int> ListInitCount()
+        {
+            list = new List<int>(count);
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(i);
+            }
+            return list;
+        }
+
+        [Benchmark]
+        public List<int> ListInitLinq()
+        {
+            return Enumerable.Range(0, count).ToList();
+        }
+
+    }
+}
